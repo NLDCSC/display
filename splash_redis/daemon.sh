55ac0d3b3d146a7761f776eb6039e6a046c147d2
Paul Tikken Laptop <paul.tikken@gmail.com>
2023-01-31 15:39:05 +0000

    function tests

---
+++ splash_redis/daemon.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+cd ..
+
+export DEBUG=True
+
+export LOG_FILE_PATH=/home/paul/PycharmProjects/display/dev_data/flask/logs
+
+export CONFIG_PATH=/home/paul/PycharmProjects/display/dev_data/flask/config
+
+export SCREENSHOT_LOCATION=/home/paul/PycharmProjects/display/dev_data/flask/screenshots
+
+export REDIS_URL=redis://localhost:6379/
+
+export SPLASH_HOST=localhost
+
+export LOG_LEVEL=DEBUG
+
+celery -A display.celery_app.display_daemon worker --loglevel=INFO -B -O fair --without-heartbeat --without-gossip --without-mingle
