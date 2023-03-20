fbef6a64ec84e727b1a1dcf74207a53bfb1452b9
Paul Tikken <paul.tikken@gmail.com>
2023-03-20 10:50:51 +0000

    minor

--- display/webapp/home/websockets.py
+++ display/webapp/home/websockets.py
@@ -172,7 +172,6 @@ def do_change_display_tab(tab_name):
                 "tab_hash": hashlib.md5(tab_name["data"].encode("utf-8")).hexdigest()[:6],
             },
             to=req_client.sid,
-            # callback=cfm_received(client_id=req_client.sid, data=tab_name["data"]),
             timeout=10,
         )
         logger.info(f"Client {req_client.sid} received data on tab: {tab_name['data']}")
