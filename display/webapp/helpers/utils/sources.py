c741fcf68d9a08e99bad01b3557802ffba853ee7
Paul Tikken <paul.tikken@gmail.com>
2023-03-21 14:37:41 +0000

    sorted all entries in display sources (both tabs as tab content)

--- display/webapp/helpers/utils/sources.py
+++ display/webapp/helpers/utils/sources.py
@@ -1,6 +1,7 @@
 import collections
 import json
 import os
+from operator import itemgetter
 
 from display.webapp.config import Config
 
@@ -20,8 +21,15 @@ def get_display_sources(add_header_tabs: bool = False):
 
         display_sources = {"none": [{}]}
 
+    # sort entries in display source
+    for key, values in display_sources.items():
+        display_sources[key] = sorted(values, key=itemgetter('header'))
+
     if add_header_tabs:
         header_display_sources = add_header_tabs_from_sources(display_sources)
+
+        for key, values in header_display_sources.items():
+            header_display_sources[key] = sorted(values, key=itemgetter('alt_header'))
 
         display_sources = {**display_sources, **header_display_sources}
 
