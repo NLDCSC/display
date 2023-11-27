1f39f9ba2901a1897868d9c45798a65e29b165b6
Paul Tikken <paul.tikken@gmail.com>
2023-11-27 10:42:41 +0000

    init script altered

--- .docker/files/init_db/init.sh
+++ .docker/files/init_db/init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-python3 -m db_migrate -u
-python3 -m scripts.setup_default_user
-python3 -m data.scripts.create_users
+FILE=INIT_COMPLETED
+
+if [ -f "$FILE" ]; then
+  echo "INIT Already performed, exiting!"
+else
+  echo "Starting DB INIT...."
+  python3 -m db_migrate -u
+  python3 -m scripts.setup_default_user
+  python3 -m data.scripts.create_users
+  touch "$FILE"
+  echo "Done with DB INIT!!"
+fi
