6f660f6a6461ef8df33c875c57506b64d1f32c17
Dion Akkerman <dionakkerman@gmail.com>
2025-04-13 23:19:53 +0200

    Add autorefresh and view sharing

--- display/webapp/home/charts.py
+++ display/webapp/home/charts.py
@@ -1,8 +1,9 @@
 import logging
 import time
-from typing import List
+import uuid
+import json
 
-from flask import render_template
+from flask import render_template, request, abort
 from flask_login import login_required
 from nldcsc.loggers.app_logger import AppLogger
 from sqlalchemy import select
@@ -10,7 +11,7 @@ from sqlalchemy import select
 from . import home
 from ..app.models import Defacements
 from ..config import Config
-from ..run import db
+from ..run import db, rediswrap
 from ...core.charts.scatter_data import ScatterChartData, DefacementsContainer
 from ...core.parsers.display_config_parser import DisplayConfigParser
 
@@ -23,7 +24,7 @@ config = Config()
 display_config_parser = DisplayConfigParser()
 
 
-def get_defacements_from_database() -> List[ScatterChartData]:
+def get_defacements_from_database() -> list[ScatterChartData]:
 
     graph_time_range = int(time.time()) - config.DISPLAY_GRAPH_TIME_RANGE * 60 * 60
 
@@ -45,6 +46,47 @@ def charts():
     display_config = display_config_parser.get_display_config_obj()
     target_count = display_config.target_count
 
-    scatter_data = get_defacements_from_database()
-
     return render_template("pages/charts.html", header="Defacement Chart", **locals())
+
+
+@home.route("_scatter_data")
+@login_required
+def get_scatter_data():
+    view_id = request.args.get("view-id", None)
+    if view_id in [None, "null"]:
+        return get_defacements_from_database()
+
+    view_name = f"defacement-view:{view_id}"
+    data = rediswrap.redis_client.hgetall(name=view_name)
+    if data is None or len(data) == 0:
+        abort(404, "View not found")
+
+    # Reset expiration on timer, apparently people are still using it...
+    rediswrap.redis_client.expire(name=view_name, time=60 * 60)
+    return {k.decode("utf-8"): json.loads(v) for k, v in data.items()}
+
+
+@home.route("/_save_custom_view", methods=["POST"])
+@login_required
+def create_custom_view():
+    view_data = request.json
+    data = view_data.get("data")
+    layout = view_data.get("layout")
+    if data is None or layout is None:
+        abort(400, description="Creating view failed - required data missing")
+
+    # Absolutely tiny chance of collisions with uuid4 but check just to be safe:
+    for attempt in range(6):
+        view_id = str(uuid.uuid4())
+        view_name = f"defacement-view:{view_id}"
+        if not rediswrap.redis_client.exists(view_name):
+            break
+    if attempt >= 5:
+        raise abort(500, description="Failed to generate unique uuid")
+
+    rediswrap.redis_client.hset(
+        name=view_name, mapping={"data": json.dumps(data), "layout": json.dumps(layout)}
+    )
+    rediswrap.redis_client.expire(name=view_name, time=60 * 60)
+
+    return {"view-id": view_id}
---
+++ display/webapp/static/js/defacements.js
@@ -0,0 +1,120 @@
+const create_plot = function (view_id) {
+    fetch(`/_scatter_data?view-id=${view_id}`, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "GET",
+    })
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                throw res
+            }
+        })
+        .then(plot_data => {
+            if (view_id === null) {
+                Plotly.newPlot('defacement_chart', plot_data, layout, options);
+            } else {
+                Plotly.newPlot('defacement_chart', plot_data["data"], plot_data["layout"], options);
+            }
+        })
+        .catch(res => {
+            if (res.status === 404) {
+                Swal.fire({
+                    title: "FAILED to fetch data!",
+                    text: "View doesn't exist (anymore).",
+                    icon: "error"
+                })
+            } else {
+                Swal.fire({
+                    title: "FAILED to fetch data!",
+                    text: "Oh no",
+                    icon: "error",
+                });
+            }
+        });
+}
+
+
+const save_view = function (gd) {
+    let plot_data = gd.data;
+    let plot_layout = gd.layout;
+    fetch("/_save_custom_view", {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({"data": plot_data, "layout": plot_layout})
+    })
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                throw res
+            }
+        })
+        .then(data => {
+            const url = `${location.protocol + '//' + location.host + location.pathname}?view-id=${data['view-id']}`;
+            navigator.clipboard.writeText(url);
+            Swal.fire({
+                title: "Url copied to clipboard!",
+                text: url,
+                icon: "success"
+            });
+        })
+        .catch(res => {
+            Swal.fire({
+                title: "FAILED to save view!",
+                text: res.status,
+                icon: "error"
+            });
+            console.log(res)
+        });
+};
+
+
+const update_plot = function () {
+    fetch(`/_scatter_data`, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "GET",
+    })
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                throw res
+            }
+        })
+        .then(plot_data => {
+            const gd = document.getElementById('defacement_chart');
+            // Maintain visibility status, otherwise it's reset on every refresh.
+            plot_data = plot_data.map((team_data, index) => {
+                team_data.visible = gd.data[index].visible
+                return team_data
+            });
+            gd.data = plot_data;
+            Plotly.redraw(gd);
+        })
+        .catch(res => {
+            if (res.status === 404) {
+                Swal.fire({
+                    title: "FAILED to refresh data!",
+                    text: "View doesn't exist (anymore).",
+                    icon: "error"
+                })
+            } else {
+                Swal.fire({
+                    title: "FAILED to refresh data!",
+                    text: "Oh no",
+                    icon: "error",
+                });
+            }
+        });
+}
+
