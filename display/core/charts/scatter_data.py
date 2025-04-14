f52b8b7441431e732e7301de1e7d17e71c6ba268
DionAkkerman <dionakkerman@gmail.com>
2025-04-14 08:21:38 +0200

    Various partner run wishes (#78) fix #23, fix #24, fix #25
    * Add autorefresh and view sharing
    
    * Make defacement tooltip clearer

--- display/core/charts/scatter_data.py
+++ display/core/charts/scatter_data.py
@@ -1,6 +1,6 @@
 import collections
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, field
+from datetime import datetime
 
 from display.webapp.app.models import Defacements
 
@@ -8,30 +8,39 @@ from display.webapp.app.models import Defacements
 @dataclass
 class ScatterChartData:
     name: str
-    x: List[str]
-    y: List[str]
+    x: list[str]
+    y: list[str]
+    hovertemplate: str = field(init=False)
     type: str = "scatter"
+
+    def __post_init__(self):
+        self.hovertemplate = f"<b>{self.name}</b><br>At: %{{x}}<br>Defacements: %{{y}}"
 
 
 @dataclass
 class DefacementsContainer:
-    data: List[Defacements]
+    data: list[Defacements]
 
     @property
-    def defacements_per_header(self) -> dict[str, List[Defacements]]:
+    def defacements_per_header(self) -> dict[str, list[Defacements]]:
         ret_dict = collections.defaultdict(list)
         for each in self.data:
             ret_dict[each.header].append(each)
         return dict(ret_dict)
 
     @property
-    def scatterchartdata_per_header(self) -> List[ScatterChartData]:
+    def scatterchartdata_per_header(self) -> list[ScatterChartData]:
         ret_list = []
         for header, defacement_data in self.defacements_per_header.items():
             ret_list.append(
                 ScatterChartData(
                     name=header,
-                    x=[x.created_at for x in defacement_data],
+                    x=[
+                        datetime.fromtimestamp(x.created_at).strftime(
+                            "%Y-%m-%dT%H:%M:%SZ"
+                        )
+                        for x in defacement_data
+                    ],
                     y=[x.count for x in defacement_data],
                 )
             )
