2a1c8935be967685b7ca7780bfa722b3ac72053a
Paul Tikken <paul.tikken@gmail.com>
2026-04-14 06:03:04 +0000

    added support for setting log purge time to a decimal value

--- display/webapp/config.py
+++ display/webapp/config.py
@@ -45,7 +45,7 @@ class Config(object):
     LOG_FILE_PATH: str = os.getenv("LOG_FILE_PATH", "/app/data/logs/")
     LOG_FILE_NAME: str = os.getenv("LOG_FILE_NAME", "display.log")
     LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO")
-    LOG_PURGE_TIME: int = int(os.getenv("LOG_PURGE_TIME", 4))  # in days
+    LOG_PURGE_TIME: int | float = os.getenv("LOG_PURGE_TIME", 4)  # in days
 
     SYSLOG_ENABLE: bool = getenv_bool("SYSLOG_ENABLE", "False")
     GELF_SYSLOG: bool = getenv_bool("GELF_SYSLOG", "True")
